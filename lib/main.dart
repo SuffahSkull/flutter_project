e1162f4fe1210a01a133a2ec1001af6e685c0cb4
WaseemAlBizreh <waseem.bez@gmail.com>
2021-12-05 12:19:36 +0200

    m

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-//Stateless Class
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
